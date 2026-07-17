cc63bfd37f614bb6c5a98e0cfb963479f562cbb3
MargXX <margeauxcorrigan@gmail.com>
2026-07-17 13:23:26 -0400

    initial testing suite, without null guards

--- Debug/debug_blink.c
+++ Debug/debug_blink.c
@@ -6,18 +6,18 @@
 
 #include "debug_blink.h"
 
-void blink_loop(uint8_t blink_rate_ms) {
+void blink_loop(uint32_t blink_rate_ms) {
     while (1) {
-        gpio_toggle(25);
-        systick_delay_ms(blink_rate_ms);
+        bm_gpio_toggle(25);
+        bm_systick_delay_ms(blink_rate_ms);
     }
 }
 
 void blink_n_times(uint8_t blink_count) {
     for (int i = 0; i < blink_count; i++) {
         bm_gpio_put(25,true);
-        systick_delay_ms(500);
+        bm_systick_delay_ms(500);
         bm_gpio_put(25,false);
-        systick_delay_ms(500);
+        bm_systick_delay_ms(500);
     }
 }
--- Debug/debug_blink.h
+++ Debug/debug_blink.h
@@ -6,17 +6,16 @@
  * to provide visual feedback during debugging and testing.
  */
 
-
-
-
 #ifndef DEBUG_BLINK_H
 #define DEBUG_BLINK_H
 
+#include "../GPIO/gpio.h"
+#include "../SysTick/systick.h"
 #include <stdint.h>
 #include <stdbool.h>
 
 //manipulates GPIO 25 (onboard LED) to toggle every blink_rate_ms miliseconds in a loop
-void blink_loop(uint8_t blink_rate_ms);
+void blink_loop(uint32_t blink_rate_ms);
 
 //manipulates GPIO 25 (onboard LED) to blink a number of times, then returns
 void blink_n_times(uint8_t blink_count);
